49de6ab4e249a8b0874d959b5ff7c7e93cf1435e
David Fry <david.fry.tv@gmail.com>
2019-10-08 19:51:34 -0700

    added styled componets and google fonts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { createGlobalStyle } from 'styled-components';
 
 function App() {
   return (
-    <div>
-      <h1>Hello Bite-size Sushi!</h1>
-    </div>
+    <>
+      <GlobalStyle />
+      <div>Hello Bite-size!</div>
+    </>
   );
 }
 
 export default App;
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: blue;
+    
+    font-family: 'Lato', sans-serif;
+  }
+   h1, h2, h3, h4, h5, h6 {
+    font-family: 'Anton', sans-serif;
+   }
+`;
