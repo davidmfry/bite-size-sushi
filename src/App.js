c6052c987f5b56a7efc252a4254cceed90c424e1
David Fry <david.fry.tv@gmail.com>
2019-10-10 07:26:07 -0700

    Added a Favicon and created a new hook called useTitle

--- src/App.js
+++ src/App.js
@@ -7,12 +7,16 @@ import Menu from './components/Menu/Menu';
 import FoodDialog from './components/FoodDialog/FoodDialog';
 import Order from './components/Order/Order';
 
+// Hooks
 import { useOpenFood } from './Hooks/useOpenFood';
 import { useOrders } from './Hooks/useOrders';
+import { useTitle } from './Hooks/useTitle';
 
 function App() {
   const openFood = useOpenFood();
   const orders = useOrders();
+  useTitle({ ...openFood, ...orders });
+
   return (
     <>
       <GlobalStyle />
